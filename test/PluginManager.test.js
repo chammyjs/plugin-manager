4c7d044962447e537dfd393a586a3727eb877b0a
Tomasz Jakut <vepomoc@gmail.com>
2017-11-23 22:08:32 +0100

    test(iterator): add tests for iterator

--- test/PluginManager.test.js
+++ test/PluginManager.test.js
@@ -6,4 +6,26 @@ describe( 'PluginManager', () => {
 	it( 'is a class', () => {
 		expect( PluginManager ).to.be.a( 'function' );
 	} );
+
+	it( 'has own property plugins', () => {
+		const pluginManager = new PluginManager();
+
+		expect( pluginManager ).to.have.own.property( 'plugins' );
+		expect( pluginManager.plugins ).to.be.an( 'array' );
+	} );
+
+	it( 'exposes iterator', () => {
+		expect( PluginManager.prototype ).to.have.property( Symbol.iterator );
+	} );
+
+	describe( 'iterator', () => {
+		it( 'iterates through plugins property', () => {
+			const pluginManager = new PluginManager();
+			const samplePlugins = [ 1, 2, 3 ];
+
+			pluginManager.plugins = samplePlugins;
+
+			expect( [ ...pluginManager ] ).to.deep.equal( samplePlugins );
+		} );
+	} );
 } );
