5c6c4b4b123f778c30ebe606780dd59b697c514f
Tomasz Jakut <vepomoc@gmail.com>
2017-12-03 21:54:42 +0100

    feat(load): initial implementation

--- src/PluginManager.js
+++ src/PluginManager.js
@@ -1,6 +1,33 @@
+// const { Plugin } = require( '@chammy/plugin-helper' );
+
+function isString( value ) {
+	return typeof value === 'string';
+}
+
+
 class PluginManager {
 	constructor() {
 		this.plugins = [];
+	}
+
+	/**
+	 * Load given plugins.
+	 *
+	 * @param {string[]} plugins Names of plugins to load.
+	 * @returns {Promise}
+	 */
+	load( plugins ) {
+		if ( !Array.isArray( plugins ) || !plugins.every( isString ) ) {
+			throw new TypeError( 'Parameter must be an array of strings' );
+		}
+
+		const loaded = plugins.map( ( plugin ) => {
+			const module = require( plugin );
+
+			return module;
+		} );
+
+		return Promise.all( loaded );
 	}
 
 	[ Symbol.iterator ]() {
--- test/PluginManager.test.js
+++ test/PluginManager.test.js
@@ -1,6 +1,11 @@
 const PluginManager = require( '../src/PluginManager' );
 const chai = require( 'chai' );
+const { join } = require( 'path' );
 const expect = chai.expect;
+
+function getPath( name ) {
+	return `${ join(  __dirname, name ) }`;
+}
 
 describe( 'PluginManager', () => {
 	it( 'is a class', () => {
@@ -18,6 +23,11 @@ describe( 'PluginManager', () => {
 		expect( PluginManager.prototype ).to.have.property( Symbol.iterator );
 	} );
 
+	it( 'exposes load method', () => {
+		expect( PluginManager.prototype ).to.have.property( 'load' );
+		expect( PluginManager.prototype.load ).to.be.a( 'function' );
+	} );
+
 	describe( 'iterator', () => {
 		it( 'iterates through plugins property', () => {
 			const pluginManager = new PluginManager();
@@ -26,6 +36,36 @@ describe( 'PluginManager', () => {
 			pluginManager.plugins = samplePlugins;
 
 			expect( [ ...pluginManager ] ).to.deep.equal( samplePlugins );
+		} );
+	} );
+
+	describe( 'load', () => {
+		it( 'accepts only array of string as parameter', () => {
+			const pluginManager = new PluginManager();
+			const invalid = [
+				'test',
+				1,
+				undefined,
+				null,
+				{},
+				[ 1, 2 ],
+				[ null ]
+			];
+
+			invalid.forEach( ( param ) => {
+				expect( () => {
+					pluginManager.load( param );
+				} ).to.throw( TypeError, 'Parameter must be an array of strings' );
+			} );
+		} );
+
+		it( 'returns Promise with resolved modules array', () => {
+			const pluginManager = new PluginManager();
+
+			return pluginManager.load( [ getPath( './fixtures/test' ) ] ).then( ( plugins ) => {
+				expect( plugins ).to.be.an( 'array' );
+				expect( plugins ).to.have.lengthOf( 1 );
+			} );
 		} );
 	} );
 } );
